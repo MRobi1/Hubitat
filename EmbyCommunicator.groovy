039f9dd82e4f942e761da58b331c8622504fad76
MRobi1 <34860474+MRobi1@users.noreply.github.com>
2020-03-15 15:13:17 -0300

    Update EmbyCommunicator.groovy

--- EmbyCommunicator.groovy
+++ EmbyCommunicator.groovy
@@ -65,8 +65,6 @@ def initialize() {
     }else{
     	getChildDevices().each { deleteChildDevice("${it.deviceNetworkId}") }
     }
-    // Just in case embyPoller has gasped it's last breath (in case it's used)
-    if(settings?.stPoller){runEvery3Hours(embyPoller)}
 }
 
 preferences {
@@ -80,7 +78,7 @@ preferences {
 }
 
 mappings {
-  path("/statechanged/:command") 	{ action: [GET: "embyExeHandler"] }
+//  path("/statechanged/:command") 	{ action: [GET: "embyExeHandler"] }
   path("/ewhset") 					{ action: [GET: "ewhset"]   }
   path("/ewh") 						{ action: [POST: "embyWebHookHandler"] }
 }
@@ -165,9 +163,6 @@ def authPage() {
 		}
     }
 }
-def authPage2() {
-    clientPage()
-}
 
 /***********************************************************
 ** CLIENTS
@@ -202,7 +197,7 @@ def clearClients() {
 def getClientList() {
     def devList = [:]
     state.embyClients.each { id, details -> devList << [ "$id": "${details.name}" ] }
-    state.playingClients.each { id, details -> devList << [ "$id": "${details.name} [►]" ] }
+//    state.playingClients.each { id, details -> devList << [ "$id": "${details.name} [►]" ] }
     return devList.sort { a, b -> a.value.toLowerCase() <=> b.value.toLowerCase() }
 }
 
@@ -210,11 +205,10 @@ def getClients(){
     // set lists
 	def isMap = state.embyClients instanceof Map
     if(!isMap){state.embyClients = [:]}
-    def isMap2 = state.playingClients instanceof Map
-    if(!isMap2){state.playingClients = [:]}
+//    def isMap2 = state.playingClients instanceof Map
+//    if(!isMap2){state.playingClients = [:]}
     // Get devices.json clients
-    getClientsXML()
-    // Request server:${settings.embyServerPort}/Sessions?DeviceId={deviceId} clients - chrome cast for example is not in devices.
+    getClientsJSON()
 	executeRequest("http://${settings.embyServerIP}:${settings.embyServerPort}/emby/Devices?&api_key=${settings.embyApiKey}", "GET")
 }
 
@@ -231,7 +225,7 @@ def executeRequest(Path, method) {
 	}
 	catch (Exception e) {log.debug "Hit Exception $e on $hubAction"}
 }
-
+/*
 def response(evt) {	 
 	// Reponse to hub from now playing request    
     def msg = parseLanMessage(evt.description);
@@ -246,17 +240,17 @@ def response(evt) {
         	else if(thing.Player.@device.text()!="")	{whatToCallMe = "${thing.Player.@device.text()}-${thing.Player.@product.text()}"}
             playingDevices << [ (thing.Player.@machineIdentifier.text()): [name: "${whatToCallMe}", id: "${thing.Player.@machineIdentifier.text()}"]]
             
-            if(settings?.stPoller){
+            if(settings?.hePoller){
     			def embyEvent = [:] << [ id: "${thing.Player.@machineIdentifier.text()}", type: "${thing.@type.text()}", status: "${thing.Player.@state.text()}", user: "${thing.User.@title.text()}" ]
                 stillPlaying << "${thing.Player.@machineIdentifier.text()}"
         		eventHandler(embyEvent)
             }
         }
-        if(settings?.stPoller){
+        if(settings?.hePoller){
         	//stop anything that's no long visible in the playing list but was playing before
         	state.playingClients.each { id, data ->
             	if(!stillPlaying.contains("$id")){
-                	def embyEvent2 = [:] << [ id: "${id}", type: "--", status: "stopped", user: "--" ]
+                	def embyEvent2 = [:] << [ id: "$playerID", type: "--", status: "stopped", user: "--" ]
                     eventHandler(embyEvent2)
                 }
             }
@@ -265,9 +259,9 @@ def response(evt) {
         state.playingClients = playingDevices
     }
 }
+*/
 
-
-def getClientsXML() {
+def getClientsJSON() {
     //getAuthenticationToken()
     def jsonDevices = [:]
     // Get from Devices List
@@ -296,6 +290,7 @@ def getClientsXML() {
 /***********************************************************
 ** INPUT HANDLERS
 ************************************************************/
+/*
 def embyExeHandler() {
 	def status = params.command
 	def userName = params.user
@@ -308,7 +303,7 @@ def embyExeHandler() {
     eventHandler(embyEvent)
 	return
 }
-
+*/
 
 def embyWebHookHandler(){
     def payloadStart = request.body.indexOf('application/json') + 78
@@ -327,15 +322,15 @@ def embyWebHookHandler(){
     //log.debug embyEvent
     eventHandler(embyEvent)
 }
-
+/*
 def embyPoller(){
-	if(settings?.stPoller){
+	if(settings?.hePoller){
     	executeRequest("/status/sessions", "GET")
     	log.warn "Emby Poller Update"
     	runOnce( new Date(now() + 10000L), embyPoller)
     }
 }
-
+*/
 
 /***********************************************************
 ** DTH OUTPUT
